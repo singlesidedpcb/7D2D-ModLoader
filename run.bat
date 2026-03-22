da901a9d0917703018dc84b24e941ae5e71e66f0
SingleSidedPCB <157340254+singlesidedpcb@users.noreply.github.com>
2026-03-22 08:58:34 -0400

    Delete run.bat

--- run.bat
+++
@@ -1,6 +0,0 @@
-@echo off
-set "script=%~dp0ModLoader.ps1"
-set "scriptdir=%~dp0"
-powershell -NoProfile -ExecutionPolicy Bypass -Command ^
- "Start-Process powershell -Verb RunAs -WorkingDirectory '%scriptdir%' -ArgumentList '-NoProfile -ExecutionPolicy Bypass -File \"%script%\"'"
-exit
